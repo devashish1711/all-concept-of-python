5ef19be91268127e180d81655f7cb097987450c0
Devashish <devinfo1711@gmail.com>
2022-04-10 15:38:50 +0530

    Initial commit

---
+++ string.py
@@ -0,0 +1,22 @@
+course = "pyhton \"programming\""
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:4])
+print(course[:5])
+print(course[:])
+print(course[1:-1])
+
+
+# \"
+# \'
+# \\
+# \n  -> new line
+
+first = "devashish"
+last = "prajapat"
+full = first + " " + last
+full = f"{first} {last}"
+print(full)
+print(len(first))
+
---
+++ string_method.py
@@ -0,0 +1,41 @@
+
+name = "   devashish prajapat"
+print(name.upper())
+print(name.title())
+print(name.strip())
+print(name.rstrip())
+print(name.find("pra"))
+print(name.replace("dev", "debh"))
+print("dev" in name)
+print("swift" not in name)
+
+print("                     this output is of line 13")
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+
+x = 10
+x +=3 
+print(x)
+
+print("                     this output is of line 27")
+import math
+print(round(2.9))
+print(abs(-2.9))
+print(math.ceil(2.2))
+
+ #  search python3 math module for knowing more about math modules
+#  bool(x)
+print("                     this output is of line 34")
+x = input("x: ")
+y = int(x) + 1
+print(f"value of y:{y} \nvalue of x:{x}")
+
+print("                     this output is of line 39")
+print(bool(""))
+print(bool(0))
